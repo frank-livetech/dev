0eae69bae35dd52ff4b4f6ad1eb2cb4eb085f008
kashif70000 <muhammadkashif70000@gmail.com>
2022-01-13 22:36:47 +0500

    ticket update changes

--- app/Http/Controllers/SystemManager/MailController.php
+++ app/Http/Controllers/SystemManager/MailController.php
@@ -939,20 +939,20 @@ class MailController extends Controller
                     for($dd = 0 ; $dd < sizeof($old_params) ; $dd++){
 
                         if($old_params[$dd]['id'] == '1'){
-                            $actions .= 'Department: '.$ticket['department_name'].' (was: '.$old_params[$dd]["data"].')\n';
+                            $actions .= 'Department: '.$ticket['department_name'].' (was: '.$old_params[$dd]["data"].')\r\n';
                         }
                         elseif($old_params[$dd]['id'] == '2'){
-                            $actions .= 'Staff: '.$ticket['assignee_name'].' (was: '.$old_params[$dd]["data"].')\n';
+                            $actions .= 'Staff: '.$ticket['assignee_name'].' (was: '.$old_params[$dd]["data"].')\r\n';
 
                         }elseif($old_params[$dd]['id'] == '3'){
-                            $actions .= 'Type: '.$ticket['type_name'].' (was: '.$old_params[$dd]["data"].')\n';
+                            $actions .= 'Type: '.$ticket['type_name'].' (was: '.$old_params[$dd]["data"].')\r\n';
 
                         }elseif($old_params[$dd]['id'] == '4'){
-                            $actions .= 'Status: '.$ticket['status_name'].' (was: '.$old_params[$dd]["data"].')\n';
+                            $actions .= 'Status: '.$ticket['status_name'].' (was: '.$old_params[$dd]["data"].')\r\n';
 
                             
                         }elseif($old_params[$dd]['id'] == '5'){
-                            $actions .= 'Priority: '.$ticket['priority_name'].' (was: '.$old_params[$dd]["data"].')\n';
+                            $actions .= 'Priority: '.$ticket['priority_name'].' (was: '.$old_params[$dd]["data"].')\r\n';
                           
                         }
 
