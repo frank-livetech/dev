877c22ea0ebac65deacefcdea77b8a0a77b59f1a
kashif70000 <muhammadkashif70000@gmail.com>
2022-01-28 17:31:06 +0500

    dev changes

--- resources/views/customer/Js/customer_tktJs.blade.php
+++ resources/views/customer/Js/customer_tktJs.blade.php
@@ -75,7 +75,7 @@
                                     let coustom_id =  full.coustom_id != null ? full.coustom_id : '-';
                                     let url = "{{route('customer.tkt_dtl', ':id')}}";
                                     url = url.replace(':id', coustom_id);
-                                    return `<div> <strong> <a href="${url}">${subject}</a> </strong> <br> ${coustom_id} </div>`;
+                                    return `<div>  ${coustom_id} </div>`;
                                 }
                             },
                             {
