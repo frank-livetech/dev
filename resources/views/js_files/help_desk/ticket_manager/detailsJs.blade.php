76d75b90b985badecabff335ffe22169904da557
kashif70000 <muhammadkashif70000@gmail.com>
2022-01-30 15:27:49 +0500

    dec changes

--- resources/views/js_files/help_desk/ticket_manager/detailsJs.blade.php
+++ resources/views/js_files/help_desk/ticket_manager/detailsJs.blade.php
@@ -1052,7 +1052,7 @@ function listReplies() {
                                     <div class="borderOne">
                                         <span class="overlayAttach"></span>
 
-                                        <img src="{{asset('public/files/replies/${ticket_details.id}/${item}')}}"  alt="">
+                                        <img src="{{asset('public/files/replies/${ticket_details.id}/${item}')}}" class=" attImg"  alt="">
                                         <span class="fileName"><img style="width:16px;height:16px;" src="{{asset('assets/images/icon/image_icon.png')}}"  alt=""> ${item}</span>
                                         <a href="{{asset('public/files/replies/${ticket_details.id}/${item}')}}" download="{{asset('public/files/replies/${ticket_details.id}/${item}')}}" class="downFile"><i class="fa fa-download"></i></a>
                                     </div>
