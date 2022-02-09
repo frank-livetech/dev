bde1722895ba2ff5b2f3f3d68751481f4aa54e30
kashif70000 <muhammadkashif70000@gmail.com>
2022-02-09 13:38:38 +0500

    dev changes

--- app/Http/Controllers/HelpdeskController.php
+++ app/Http/Controllers/HelpdeskController.php
@@ -2386,8 +2386,8 @@ class HelpdeskController extends Controller
                     $oldval = '';
                     if($request->has('data_id')) $data_id = $request->data_id;
                     if($request->has('oldval')) $oldval = $request->oldval;
-                    $user->email = \Auth::user()->email;
-                    $this->sendNotificationMail($ticket->toArray(), $request->template, '', '', $request->action, $data_id,$user->email,$oldval);
+                    $email = \Auth::user()->email;
+                    $this->sendNotificationMail($ticket->toArray(), $request->template, '', '', $request->action, $data_id,$email,$oldval);
                 } else {
                     $response['message'] = 'Ticket is either deleted or not exists!';
                 }
