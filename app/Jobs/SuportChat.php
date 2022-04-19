49d4c83fa5964b3b68c59ee953ba297864cfdc49
mhamzaq869 <mahzamq869@gmail.com>
2022-04-19 12:16:51 +0500

    Image Upload in Support Chat

---
+++ app/Jobs/SuportChat.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Pusher\Pusher;
+
+class SuportChat implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $message,$reciever,$sender;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($message,$reciever_id,$sender)
+    {
+        $this->message = $message;
+        $this->reciever = $reciever_id;
+        $this->sender = $sender;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            [
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true
+            ]
+        );
+
+        $data = [
+            "message" => $this->message,
+            "reciever_id" => (int) $this->reciever,
+            "sender" => $this->sender,
+        ];
+
+        $pusher->trigger('support-chat.'.(int) $this->reciever, 'support-chat-event', $data);
+
+    }
+}
