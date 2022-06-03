655fa1005b62a61f997e7a0193c69af3e4e77527
unknown <abdulrehman24>
2022-06-03 22:03:26 +0500

    chat changes

--- resources/views/js_files/help_desk/ticket_manager/detailsJs.blade.php
+++ resources/views/js_files/help_desk/ticket_manager/detailsJs.blade.php
@@ -1351,7 +1351,7 @@ function parserEmbeddedImages(){
 
 function showAttachedImage(id, item) {
     let img = `<img src="{{asset('storage/tickets/${id}/${item}')}}" class="w-100 h-100">`;
-    let downloadimg = `<a class="btn btn-primary waves-effect waves-float waves-light" href="{{asset('storage/tickets/${id}/${item}')}}" download><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg> Download</a>`
+    let downloadimg = `<a class="btn btn-primary waves-effect waves-float waves-light" href="{{asset('storage/tickets/${id}/${item}')}}" download><svg style="color: #fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg><span style="color: #fff"> Download</span></a>`
     $('.showDefaultPreview').html(img);
     $('.DownloadImage').html(downloadimg);
     $("#defaultPreview").modal('show');
