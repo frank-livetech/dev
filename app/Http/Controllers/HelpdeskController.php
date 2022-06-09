0bc8272f34c81a28c6e23415b0c9cca56e8bf439
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-06-09 21:37:34 +0500

    change in status

--- app/Http/Controllers/HelpdeskController.php
+++ app/Http/Controllers/HelpdeskController.php
@@ -917,8 +917,8 @@ class HelpdeskController extends Controller
         $flagged_tickets_count = Tickets::where([ ['is_flagged', 1] , ['is_deleted', 0] ,['tickets.trashed', 0] , ['is_pending' ,0] ,['tickets.status', '!=', $closed_status_id] ])->count();
 
         $open_ticket_count = Tickets::
-        when($statusOrUser == 'customer', function($q) use ($cid , $open_status) {
-            return $q->where('tickets.customer_id', $cid)->where('status' ,'!=', $closed_status->id);;
+        when($statusOrUser == 'customer', function($q) use ($cid , $open_status , $closed_status_id) {
+            return $q->where('tickets.customer_id', $cid)->where('status' ,'!=', $closed_status_id);
         })
         ->when($statusOrUser == 'staff', function($q) use ($sid) {
             return $q->where('tickets.assigned_to', $sid);
