c3161f77451d489d4f8ce05b65384f33c850c6b6
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-06-23 17:40:10 +0500

    dev changes

--- app/Http/Controllers/HelpdeskController.php
+++ app/Http/Controllers/HelpdeskController.php
@@ -1117,10 +1117,14 @@ class HelpdeskController extends Controller
 
                     if($dd_values[$dd]['id'] == 1){
                         $data['dept_id'] = $dd_values[$dd]['new_data'] ;
+                        $ticket->dept_id = $dd_values[$dd]['new_data'];
+
                         $data['action_performed'] = 'Ticket (<a href="'.url('ticket-details').'/' .$ticket->coustom_id.'">'.$ticket->coustom_id.'</a> ) department changed from: '.$ticket->department_name.' to: '.$dd_values[$dd]['new_text'];
                         $message .= '<strong> Department :</strong> '. $dd_values[$dd]['new_text'] .' (was : '. $ticket->department_name .')';  
                     }elseif($dd_values[$dd]['id'] == 2){
                         $data['assigned_to'] = $dd_values[$dd]['new_data'] ;
+                        $ticket->assigned_to = $dd_values[$dd]['new_data'];
+
                         $data['action_performed'] = 'Ticket (<a href="'.url('ticket-details').'/' .$ticket->coustom_id.'">'.$ticket->coustom_id.'</a> ) owner changed from: '. $ticket->creator_name .' to: '. $dd_values[$dd]['new_text'];
                         $message .= '<strong> Tech :</strong> '. $dd_values[$dd]['new_text'] .' (was : '. $ticket->creator_name .')';  
                     }elseif($dd_values[$dd]['id'] == 3){
@@ -1129,6 +1133,8 @@ class HelpdeskController extends Controller
                         $message .= '<strong> Type :</strong> '. $dd_values[$dd]['new_text'] .' (was : '. $ticket->type_name .')';  
                     }elseif($dd_values[$dd]['id'] == 4){
                         $data['status'] = $dd_values[$dd]['new_data'] ;
+                        $ticket->status = $dd_values[$dd]['new_data'];
+
                         $data['action_performed'] = 'Ticket (<a href="'.url('ticket-details').'/' .$ticket->coustom_id.'">'.$ticket->coustom_id.'</a> ) status changed from: '. $ticket->status_name .' to: '. $dd_values[$dd]['new_text'];
                         $message .= '<strong> Status :</strong> '. $dd_values[$dd]['new_text'].' (was : '. $ticket->status_name .')';  
                         $os = TicketStatus::where('id',$dd_values[$dd]['new_data'])->first();
@@ -1139,6 +1145,7 @@ class HelpdeskController extends Controller
                         }
                     }elseif($dd_values[$dd]['id'] == 5){
                         $data['priority'] = $dd_values[$dd]['new_data'] ;
+                        $ticket->priority = $dd_values[$dd]['new_data'];
                         $data['action_performed'] = 'Ticket (<a href="'.url('ticket-details').'/' .$ticket->coustom_id.'">'.$ticket->coustom_id.'</a> ) priority changed from: '. $ticket->priority_name .' to: '.  $dd_values[$dd]['new_text'];
                         $message .= '<strong> Priority :</strong> '.  $dd_values[$dd]['new_text'] .' (was : '. $ticket->priority_name .')';  
                     }
