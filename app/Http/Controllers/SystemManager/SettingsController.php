2ebcbe23ae70a06d529d09a9007dc0520b0aee0d
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-06-25 21:37:27 +0500

    spam user changes

--- app/Http/Controllers/SystemManager/SettingsController.php
+++ app/Http/Controllers/SystemManager/SettingsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Spatie\Permission\Models\Role;
 use App\Models\SystemManager\Feature;
-use App\Models\{SystemSetting , TicketSettings , SlaPlan , ProjectType , BrandSettings , Departments, TicketStatus, TicketPriority, TicketType, CustomerType, ResponseTemplate , ResTemplateCat, Notification, SpamUser, DispatchStatus, Tasks,Tickets, DepartmentAssignments};
+use App\Models\{SystemSetting ,Customer, TicketSettings , SlaPlan , ProjectType , BrandSettings , Departments, TicketStatus, TicketPriority, TicketType, CustomerType, ResponseTemplate , ResTemplateCat, Notification, SpamUser, DispatchStatus, Tasks,Tickets, DepartmentAssignments};
 use Exception;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\{Session, DB , Auth, Http};
