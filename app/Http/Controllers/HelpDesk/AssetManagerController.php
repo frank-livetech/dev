2954d6efd5f329a9e81fc6b38221b52174c9d68a
mhamzaq869 <mhamzaq869@gmail.com>
2022-06-29 15:15:13 +0500

    assets form field realtime and title of template both are done

--- app/Http/Controllers/HelpDesk/AssetManagerController.php
+++ app/Http/Controllers/HelpDesk/AssetManagerController.php
@@ -335,7 +335,7 @@ class AssetManagerController extends Controller
                 $response['message'] = 'Asset Deleted Successfully!';
                 $response['status_code'] = 200;
                 $response['success'] = true;
-                $response['data'] = AssetFields::where('id',$request->field_id)->where("is_deleted",0)->first();
+                $response['data'] = AssetFields::where('asset_forms_id', $request->template_id)->get();
                 return response()->json($response);
             }else{
                 if($request->has('field_id') && $request->field_id == 0){
@@ -368,7 +368,7 @@ class AssetManagerController extends Controller
                 $response['status_code'] = 200;
                 $response['success'] = true;
                 // $response['data'] = AssetFields::where('id',$request->field_id)->where("is_deleted",0)->first();
-                $response['data'] = AssetForms::with('fields')->where('is_deleted', 0)->get();
+                $response['data'] = AssetFields::where('asset_forms_id', $request->template_id)->get();
                 return response()->json($response);
             }
 
