6f7e43eaa19fff68af4a7d44e60c8f579284578b
unknown <abdulrehman24>
2022-06-29 21:13:35 +0500

    changes

--- resources/views/auth/userexpired.blade.php
+++ resources/views/auth/userexpired.blade.php
@@ -6,13 +6,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="PIXINVENT">
     <title>Link Expired</title>
     @php
-    $file_path = $live->sys_value == 1 ? 'public/' : '/';
-    $path = $live->sys_value == 1 ? 'public/system_files/' : 'system_files/';
+        $file_path = $live->sys_value == 1 ? 'public/' : '/';
+        $path = $live->sys_value == 1 ? 'public/system_files/' : 'system_files/';
     @endphp
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('files/brand_files')}}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
