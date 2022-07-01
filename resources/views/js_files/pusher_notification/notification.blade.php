b493ab408854ef51c6d5693abcd6dec232d08393
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-07-01 23:59:01 +0500

    changes in cc bcc

--- resources/views/js_files/pusher_notification/notification.blade.php
+++ resources/views/js_files/pusher_notification/notification.blade.php
@@ -3,21 +3,30 @@
     // Enter a unique channel you wish your users to be subscribed in.
     var channel = pusher.subscribe('notification.'+`{{Auth::id()}}`);
     // var presenceChannel = pusher.subscribe('presenceChannelName');
-    console.log(channel);
-    // if(is_online_notif == 0){
-        
-    // }else if(is_online_notif == ''){
-    //     $.ajax({
-    //         url: "{{route('make.online.user')}}",
-    //         dataType: "json",
-    //         type: "Post",
-    //         async: true,
-    //         data: { _token: "{{csrf_token()}}",online:false},
-    //         success: function (data) {
-    //             console.log(data)
-    //         },
-    //     });
-    // }
+
+    if(is_online_notif == 0){
+        $.ajax({
+            url: "{{route('make.online.user')}}",
+            dataType: "json",
+            type: "Post",
+            async: true,
+            data: { _token: "{{csrf_token()}}",online:true},
+            success: function (data) {
+                console.log(data)
+            },
+        });
+    }else if(is_online_notif == ''){
+        $.ajax({
+            url: "{{route('make.online.user')}}",
+            dataType: "json",
+            type: "Post",
+            async: true,
+            data: { _token: "{{csrf_token()}}",online:false},
+            success: function (data) {
+                console.log(data)
+            },
+        });
+    }
 
 
     channel.bind("notification-event", (data) => {
@@ -32,8 +41,22 @@
                 $(".noti_count").text( parseInt(msg_counter) + 1 );
                 $('.noti_count').addClass('badge rounded-pill bg-danger badge-up ');
 
+                if($notify.noti_title == 'LoggedInUser'){
+                    $.ajax({
+                        url: "{{route('show.all.user')}}",
+                        dataType: "json",
+                        type: "get",
+                        async: true,
+                        success: function (users) {
+                            for (const user of users) {
+                                $('#user-status-'+notify.sender_id).html('<span class="avatar-status-online"></span>');
+                            }
+                        },
 
-                appendNotification(notify.noti_icon , notify.noti_title , notify.noti_desc)
+                    });
+                    
+                }else{
+                    appendNotification(notify.noti_icon , notify.noti_title , notify.noti_desc)
 
                 // if(notify.noti_desc != null && notify.noti_title != null) {
                     toastr['info']( notify.noti_desc , notify.noti_title, {
@@ -43,6 +66,9 @@
                     });
 
                     jQuery("#msg_my_audio")[0].play();
+                }
+
+                
                 // }
             }
         }
