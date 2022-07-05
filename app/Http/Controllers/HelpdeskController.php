75a37a64df307e0af59cdb23158f805303037f5d
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-07-05 13:14:22 +0500

    dev changse

--- app/Http/Controllers/HelpdeskController.php
+++ app/Http/Controllers/HelpdeskController.php
@@ -267,7 +267,8 @@ class HelpdeskController extends Controller
                     sendNotificationToAdmins($slug , $type , $title ,  $desc);
 
                 }
-
+                $data['ticket_detail'] = $this->replaceBodyShortCodes($data['ticket_detail'] , $ticket);
+                
                 $data['updated_at'] = Carbon::now();
                 $data['updated_by'] = \Auth::user()->id;
 
