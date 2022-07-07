a6af2b482776bd89e00daa5577cf9cb72f61c395
mhamzaq869 <mhamzaq869@gmail.com>
2022-07-07 17:45:44 +0500

    asset import/export done

--- app/Imports/AssetFieldsImport.php
+++ app/Imports/AssetFieldsImport.php
@@ -3,41 +3,33 @@
 namespace App\Imports;
 
 use App\Models\AssetFields;
-use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithStartRow;
-use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class AssetFieldsImport implements ToModel, WithHeadingRow
+
+class AssetFieldsImport implements ToCollection, WithHeadingRow
 {
     protected $asset_type;
 
-    public function  __construct(int $asset_type) {
+    public function  __construct(int $asset_type)
+    {
         $this->asset_type= $asset_type;
     }
-   /**
+
+    public function headingRow(): int
+    {
+        return 1;
+    }
+
+    /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new AssetFields([
-            'asset_forms_id' => $this->asset_type,
-            'label' => $row['label'],
-            'type' =>  $row['type'],
-            'placeholder' => $row['placeholder'],
-            'description' => $row['description'],
-            'required' => $row['required'] ?? 0,
-            'is_multi' => $row['is_multi'] ?? 0,
-            'copy_icon' => $row['copy_icon'] ?? 0,
-            'options' => $row['options'],
-            'col_width' => $row['col_width'],
-            'created_by' => $row['created_by'],
-            'updated_by' => $row['updated_by'],
-            'deleted_at' => $row['deleted_at'],
-            'deleted_by' => $row['deleted_by'],
-            'is_deleted' => $row['is_deleted'] ?? 0,
-        ]);
+        DB::table('asset_records_'.$this->asset_type)->insert($rows->toArray());
     }
 }
