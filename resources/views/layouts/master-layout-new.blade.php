a0923ea8dc98571527fcdcd6c37cd9f4a60f59fd
mhamzaq869 <mhamzaq869@gmail.com>
2022-07-12 13:42:48 +0500

    helpdesk

--- resources/views/layouts/master-layout-new.blade.php
+++ resources/views/layouts/master-layout-new.blade.php
@@ -249,14 +249,15 @@
                         </a>
                     </li>
                 @endif
-                <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon" data-feather="search"></i></a>
+                {{-- <li class="nav-item nav-search">
+                    <a class="nav-link nav-link-search"><i class="ficon" data-feather="search"></i></a>
                     <div class="search-input">
                         <div class="search-input-icon"><i data-feather="search"></i></div>
                         <input class="form-control input" type="text" placeholder="Explore Vuexy..." tabindex="-1" data-search="search">
                         <div class="search-input-close"><i data-feather="x"></i></div>
                         <ul class="search-list search-list-main"></ul>
                     </div>
-                </li>
+                </li> --}}
                 <li class="nav-item dropdown dropdown-notification me-25">
                     <a class="nav-link" href="#" data-bs-toggle="dropdown">
                         <i class="ficon" data-feather="bell"></i>
