1659b1381acf18ff2be3d0a69a12472b1fd29c24
Muhammad Kashif <muhammadkashif70000@gmail.com>
2022-07-18 12:10:30 +0500

    dev changes

--- app/Http/Controllers/SystemManager/MailController.php
+++ app/Http/Controllers/SystemManager/MailController.php
@@ -1512,6 +1512,37 @@ class MailController extends Controller
 
                     }
                 }
+                
+                               
+                // Create a DOMDocument and load the HTML.
+                $dom = new \DOMDocument();
+                $dom->loadHTML($reply_content);
+                
+                
+                
+                // Get all the images. 
+                $images = $dom->getElementsByTagName('img');
+                
+                // Loop the images.
+                foreach ($images as $image) {
+                    $src = $image->getAttribute('src');
+                    // Create the span wrapper.
+                    $span = $dom->createElement('a');
+                    $span->setAttribute('target', '_blank');
+                    $span->setAttribute('href', $src);
+                    
+                    // Clone the span if we need to use it often.
+                    $span_clone = $span->cloneNode();
+                    
+                    // Replace the image tag with the span tag.
+                    $image->parentNode->replaceChild($span_clone, $image);
+                    
+                    // Add the image tag as a child of the new span tag.
+                    $span_clone->appendChild($image);
+                }
+                
+                // Get your HTML with saveHTML()
+                $reply_content = $dom->saveHTML();
 
                 $reply_content =  $bbcode->convertToHtml($reply_content);
                 // dd($reply_content);
@@ -2167,7 +2198,7 @@ class MailController extends Controller
                 $template = str_replace($value->code, '', $template);
             }
         }
-
+            // dd(html_entity_decode($template));
         return html_entity_decode($template);
 
 
