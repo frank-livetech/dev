9df8f314f58886312d763801f040448c73b1c464
mhamzaq869 <mhamzaq869@gmail.com>
2022-07-20 15:30:11 +0500

    fd

--- resources/views/js_files/help_desk/asset_manager/actionsJs.blade.php
+++ resources/views/js_files/help_desk/asset_manager/actionsJs.blade.php
@@ -98,6 +98,24 @@ function get_asset_table_list() {
                     },
                     {
                         "render": function(data, type, full, meta) {
+                            if(full.created_by_name != null) {
+                                return full.created_by_name;
+                            }else{
+                                return '-';
+                            }
+                        }
+                    },
+                    {
+                        "render": function(data, type, full, meta) {
+                            if(full.updated_by_name != null) {
+                                return full.updated_by_name;
+                            }else{
+                                return '-';
+                            }
+                        }
+                    },
+                    {
+                        "render": function(data, type, full, meta) {
                             return `
                                 <div class="d-flex justify-content-center">
                                     <button onclick="editAsset(${full.id})" type="button" class="btn btn-icon rounded-circle btn-outline-success waves-effect" style="padding: 0.715rem 0.936rem !important;">
